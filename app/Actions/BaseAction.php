31a9c3ad399e13c3f86ab3e34065602cfaecfd3e
David Adokuru <45779326+adokuru@users.noreply.github.com>
2025-09-23 09:57:09 +0100

    Update BaseAction.php

--- app/Actions/BaseAction.php
+++ app/Actions/BaseAction.php
@@ -7,12 +7,18 @@ abstract class BaseAction
 
     /**
      * Handle the action execution with error handling.
+     * 
+     * @param mixed ...$arguments
      *
+     * @throws \Exception
+     * 
+     * 
      * @return array
      */
     public function handle(...$arguments): array
     {
         try {
+
             // Defer to child class implementation; pass through any arguments
             $result = $this->execute(...$arguments);
 
